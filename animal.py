8b7b116e7b6a63b9da95f8270731a85548038f76
Phil Kessler <kesslerp@oregonstate.edu>
2022-09-27 13:07:55 -0700

    1. Input and a print

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Phillip Kessler
+# GitHuh Username: PhilKessler
+# Date: 09/27/2022
+# Description: Ask the user for their favorite animal and that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
